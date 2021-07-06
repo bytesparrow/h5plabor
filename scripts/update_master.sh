77f4e4642fa34d546ceda42fe535c738015d6431
null <null@users.noreply.github.com>
2021-07-06 15:47:27 +0200

    fixes update_master

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -2,9 +2,9 @@
 #environment needs some infos about pathes - keep line! ("." means "source")
 . $( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/get-drush-include-path.sh
 
-drush vset maintenance_mode 1
+drush sset system.maintenance_mode 1
 
 drush cr 
-drush fr
+drush updatedb
 
-drush vset maintenance_mode 0
+drush sset system.maintenance_mode 0
